bba52ecbb034d0d6202f84f3693258d0f8b34b6d
Civan Elismail <157229288+jwanismail@users.noreply.github.com>
2026-02-20 22:31:35 +0300

    fix: cloudinary v2 upgrade ve config debug

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,10 +1,16 @@
-const cloudinary = require('cloudinary').v2;
+const { v2: cloudinary } = require('cloudinary');
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
+
+console.log('Cloudinary config:', {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'NOT SET',
+  api_key: process.env.CLOUDINARY_API_KEY ? '***SET***' : 'NOT SET',
+  api_secret: process.env.CLOUDINARY_API_SECRET ? '***SET***' : 'NOT SET',
 });
 
 module.exports = cloudinary;
-
